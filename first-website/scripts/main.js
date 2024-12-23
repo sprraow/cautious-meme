b3a8fcb9880705913cdfc246a64cfb984f116314
sparr0w24 <shaminsmashalkar@gmail.com>
2024-12-22 18:27:32 -0800

    enter name

--- first-website/scripts/main.js
+++ first-website/scripts/main.js
@@ -8,3 +8,32 @@ myImage.addEventListener("click", () => {
     myImage.setAttribute("src", "images/pacman-title-lol.jpg");
   }
 });
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Enter yo name");
+    if (myName){
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Pacman? no... , ${myName}man`;
+    } else {
+        alert("do it now");
+        setUserName();
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName(); 
+} else{
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hey their, ${storedName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
+
+
+
+
